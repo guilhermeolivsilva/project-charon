5d834db2c6d340ed1bb023a72a66c5390f8bb1ca
Guilherme Silva <guilherme.olivsilva01@gmail.com>
2024-01-18 18:48:26 -0300

    enhancement: the default Node.value is now -1, for consistency with the SyntaxParser

--- src/abstract_syntax_tree.py
+++ src/abstract_syntax_tree.py
@@ -12,11 +12,11 @@ class Node:
     kind : str
         The Node kind. Must be VAR, CST, ADD, SUB, LT, SET, IF1, IF2, WHILE,
         DO, EMPTY, SEQ, EXPR, or PROG.
-    value : int, optional (default = 0)
-        The value the Node holds, if any. Defaults to zero.
+    value : int, optional (default = -1)
+        The value the Node holds, if any. Defaults to -1.
     """
 
-    def __init__(self, id: int, kind: str, value: int = 0) -> None:
+    def __init__(self, id: int, kind: str, value: int = -1) -> None:
         self.id: int = id
         self.kind: str = kind
         self.value: int = value
@@ -72,7 +72,7 @@ class AbstractSyntaxTree:
     """
 
     def __init__(self) -> None:
-        self.root: Node = Node(id=0, kind="PROG", value=0)
+        self.root: Node = Node(id=0, kind="PROG", value=-1)
 
     # TODO: assert no IDs are duplicate.
     # TODO: implement a tree traversal method.
