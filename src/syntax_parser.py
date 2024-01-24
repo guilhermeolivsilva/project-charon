fdf580f7126d7d681ced645e13c9d84cefd8ab96
Guilherme Silva <guilherme.olivsilva01@gmail.com>
2024-01-24 18:53:47 -0300

    feat (syntax_parser): added parenthesis_expression

--- src/syntax_parser.py
+++ src/syntax_parser.py
@@ -33,10 +33,7 @@ def uses_global_id(func: callable) -> callable:
         try:
             result = func(cls, *args, **kwargs)
         except Exception:
-            print(
-                "Could not run decorated function.",
-                "The ID counter was not affected."
-            )
+            print("Could not run decorated function. The ID counter was not affected.")
         else:
             cls.global_id_manager += 1
 
@@ -60,25 +57,44 @@ class SyntaxParser:
         self._global_id_manager = value
 
     @uses_global_id
-    def term(self, symbol: str, value: int) -> Node:
+    def term(
+        self,
+        symbol: str or list = None,
+        value: int = None,
+        lhs: Node = None,
+        rhs: Node = None,
+    ) -> Node:
         """
         Generate a `term` for the AST.
 
         Terms are either a variable, a constant, or a paranthesis expression.
 
+        If a variable or a constant, then only the `symbol` and `value`
+        parameters should be set. If a parenthesis expression, then only the
+        `lhs` and `rhs` parameters should be set.
+
         Parameters
         ----------
         symbol : str
-            The symbol to parse. If not "ID" or "INT", creates a
+            The symbol to parse. If `None`, creates a
             `parenthesis_expression`.
         value : int
             The value to be stored in the Node.
+        lhs : Node
+            The Node that represents the left hand side term inside the
+            parenthesis expression.
+        rhs : Node
+            The Node that represents the right hand side term inside the
+            parenthesis expression.
 
         Returns
         -------
         term_node : Node
             The new `term` generated Node.
         """
+
+        _err_msg = "Not all parameters must be set simultaneously."
+        assert not all([symbol, value, lhs, rhs]), _err_msg
 
         symbol_map = {
             "ID": Node(id=self.global_id_manager, kind="VAR", value=value),
@@ -88,9 +104,49 @@ class SyntaxParser:
         try:
             term_node = symbol_map[symbol]
         except KeyError:
-            term_node = self.parenthesis_expression()
+            term_node = self.parenthesis_expression(symbol, lhs, rhs)
 
         return term_node
+
+    def parenthesis_expression(self, symbol: list, lhs: Node, rhs: Node) -> Node:
+        """
+        Generate a `parenthesis_expression` for the AST.
+
+        Parameters
+        ----------
+        symbol : list
+            A list of symbols to parse. Must be as it follows: ["LPAR",
+            <symbol>, "RPAR"], where <symbol> is any symbol from the lexer.
+        lhs : Node
+            The Node that represents the left hand side term inside the
+            parenthesis expression.
+        rhs : Node
+            The Node that represents the right hand side term inside the
+            parenthesis expression.
+
+        Returns
+        -------
+        parenthesis_expression : Node
+            The Node that represents the parenthesis expression.
+
+        Raises
+        ------
+        AssertionError
+            Raised if
+            - there is no `LPAR` in the `symbol` list.
+            - there is no `RPAR` in the `symbol` list.
+            - the `RPAR` comes before the `LPAR` in the `symbol` list.
+        """
+
+        lpar, expression_symbol, rpar = symbol
+
+        assert lpar == "LPAR", "Missing left parenthesis symbol (LPAR)"
+        assert rpar == "RPAR", "Missing right parenthesis symbol (RPAR)"
+
+        _err_msg = "LPAR must come before RPAR."
+        assert symbol.index(lpar) < symbol.index(rpar), _err_msg
+
+        return self.expression(expression_symbol, lhs, rhs)
 
     @uses_global_id
     def sum(self, symbol: str, lhs: Node, rhs: Node) -> Node:
@@ -116,6 +172,7 @@ class SyntaxParser:
         sum_node : Node
             The new `sum` generated Node.
         """
+
         symbol_map = {
             "PLUS": Node(id=self.global_id_manager, kind="ADD", value=-1),
             "MINUS": Node(id=self.global_id_manager, kind="SUB", value=-1),
@@ -130,7 +187,7 @@ class SyntaxParser:
         sum_node.add_child(rhs)
 
         return sum_node
-    
+
     @uses_global_id
     def comparison(self, lhs: Node, rhs: Node) -> Node:
         """
@@ -162,9 +219,9 @@ class SyntaxParser:
         comparison_node.add_child(rhs)
 
         return comparison_node
-    
+
     @uses_global_id
-    def expression(self, lhs: Node, rhs: Node) -> Node:
+    def expression(self, symbol: str, lhs: Node, rhs: Node) -> Node:
         """
         Generate a `expression` for the AST.
 
@@ -183,6 +240,9 @@ class SyntaxParser:
         expression_node : Node
             The new `expression` generated Node.
         """
+        if symbol != "ID":
+            return self.comparison(lhs=lhs, rhs=rhs)
+
         expression_node = Node(id=self.global_id_manager, kind="SET", value=-1)
 
         lhs.add_parent(expression_node)
@@ -192,7 +252,3 @@ class SyntaxParser:
         expression_node.add_child(rhs)
 
         return expression_node
-
-    @uses_global_id
-    def parenthesis_expression(self) -> Node:
-        raise NotImplementedError()
