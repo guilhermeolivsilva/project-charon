51fc9c5ec4a9572ed31bfb434e3924056cd6ec6e
Guilherme Silva <guilherme.olivsilva01@gmail.com>
2024-02-05 19:38:41 -0300

    docs (code_generator): added missing docstrings

--- src/code_generator.py
+++ src/code_generator.py
@@ -1,46 +1,55 @@
+"""Implement a code generator for the virtual machine."""
+
 from .abstract_syntax_tree import Node
 from .syntax_parser import SyntaxParser
 
 
 class CodeGenerator:
+    """
+    Code Generator that generates instructions for the virtual machine from
+    Abstract Syntax Tree (AST) Nodes.
+    """
+
     def __init__(self) -> None:
         self.parser: SyntaxParser = SyntaxParser()
         self.code_collection: list = []
 
     def __str__(self) -> str:
+        """
+        Implement a string representation of a CodeGenerator object.
+
+        This method is internally invoked when using `print(codegen_obj)`.
+
+        Returns
+        -------
+        _str : str
+            The string representation of a CodeGenerator object.
+        """
+
         return "\n".join(
             f"Instruction: {instruction}, Node: ({node})"
             for instruction, node in self.code_collection
         )
 
     def generate_code(self, node: Node) -> None:
+        """
+        Generate code from a Node in the Abstract Syntax Tree.
+
+        Parameters
+        ----------
+        node : Node
+            A Node from the AST.
+        """
+
         instruction_map = {
-            "VAR": (
-                self.parse_simple_node,
-                {"instruction": "IFETCH"}
-            ),
-            "CST": (
-                self.parse_simple_node,
-                {"instruction": "IPUSH"}
-            ),
-            "ADD": (
-                self.parse_simple_node,
-                {"instruction": "IADD"}
-            ),
-            "SUB": (
-                self.parse_simple_node,
-                {"instruction": "ISUB"}
-            ),
-            "LT": (
-                self.parse_simple_node,
-                {"instruction": "ILT"}
-            ),
+            "VAR": (self.parse_simple_node, {"instruction": "IFETCH"}),
+            "CST": (self.parse_simple_node, {"instruction": "IPUSH"}),
+            "ADD": (self.parse_simple_node, {"instruction": "IADD"}),
+            "SUB": (self.parse_simple_node, {"instruction": "ISUB"}),
+            "LT": (self.parse_simple_node, {"instruction": "ILT"}),
             "SET": (self.parse_set_node, {}),
             "IF": (self.parse_if_node, {}),
-            "IFELSE": (
-                self.parse_if_node,
-                {"is_if_else": True}
-            ),
+            "IFELSE": (self.parse_if_node, {"is_if_else": True}),
         }
 
         handler, kwargs = instruction_map[node.kind]
@@ -48,18 +57,52 @@ class CodeGenerator:
         handler(node=node, **kwargs)
 
     def parse_simple_node(self, node: Node, instruction: str) -> None:
+        """
+        Generate code from a simple Node.
+
+        A simple Node's kind is either `VAR`, `CST`, `ADD`, `SUB` or `LT`.
+
+        Parameters
+        ----------
+        node : Node
+            The Node object to parse.
+        instruction : str
+            The instruction to add to the `code_collection`.
+        """
+
         for child in node.children:
             self.generate_code(child)
 
         self.code_collection.append((instruction, node))
 
     def parse_set_node(self, node: Node, **kwargs) -> None:
+        """
+        Generate code from a `SET` kind Node.
+
+        Parameters
+        ----------
+        node : Node
+            The `SET` Node to parse.
+        """
+
         lhs, rhs = node.children
         self.generate_code(rhs)
 
         self.code_collection.append(("ISTORE", lhs))
 
     def parse_if_node(self, node: Node, is_if_else: bool = False, **kwargs) -> None:
+        """
+        Generate code from an `IF` or `IFELSE` kind Node.
+
+        Parameters
+        ----------
+        node : Node
+            The `IF` or `IFELSE` Node to parse.
+        is_if_else : bool, optional (default = False)
+            A flag that indicates whether it is an `IF` or `IFELSE` Node. Set
+            it to `True` if it is `IFELSE`. Defaults to `False`.
+        """
+
         if is_if_else:
             expr, if_statement, else_statement = node.children
         else:
