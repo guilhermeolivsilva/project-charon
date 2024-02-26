be00d763498240e0aad55d38e4a9d2087792c653
Guilherme Silva <guilherme.olivsilva01@gmail.com>
2024-02-26 15:46:10 -0300

    feat (lexer): added source code preprocessor

--- src/lexer.py
+++ src/lexer.py
@@ -18,7 +18,7 @@ class Lexer:
             List of symbols that represent the source code.
         """
 
-        return list(map(Lexer.parse_word, source_code))
+        return list(map(Lexer.parse_word, Lexer.preprocess_source_code(source_code)))
 
     @classmethod
     def parse_word(cls, word: str) -> tuple[str, int]:
@@ -44,8 +44,6 @@ class Lexer:
         """
 
         symbol_map = {
-            " ": "",
-            "\n": "",
             "do": "DO_SYM",
             "while": "WHILE_SYM",
             "if": "IF_SYM",
@@ -76,3 +74,45 @@ class Lexer:
                 raise SyntaxError("The given input is not supported.")
 
         return (symbol, value)
+    
+    @classmethod
+    def preprocess_source_code(cls, source_code: str) -> list[str]:
+        """
+        Preprocess the source code and return its of words and characters.
+
+        This method is intended to handle reserved words, spaces, line breaks
+        and other style-related issues.
+
+        Parameters
+        ----------
+        source_code : str
+            The source code to preprocess.
+
+        Returns
+        -------
+        preprocessed_source_code : list of str
+            A list of words and individual characters obtained from the source
+            code.
+        """
+
+        # Remove line breaks
+        source_code = source_code.replace("\n", "")
+
+        # Tweak braces, parenthesis and semicolons before splitting the string
+        # by blank spaces
+        tokens_to_tweak = ["(", ")", "{", "}", ";"]
+        for token in tokens_to_tweak:
+            source_code = source_code.replace(token, f" {token} ")
+
+        # Split the code word by word (or character by character)
+        source_code = source_code.split(" ")
+
+        # Remove empty tokens
+        preprocessed_source = list(
+            filter(
+                lambda x: x if len(x) > 0 else None,
+                source_code
+            )
+        )
+
+        return preprocessed_source
