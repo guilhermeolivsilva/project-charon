6e43a9e8924635cf0f35361f1f29d17b89133f8c
Guilherme Silva <guilherme.olivsilva01@gmail.com>
2024-04-23 12:18:02 -0300

    feat! (nodes/node): AST nodes no longer have generic children attribute

--- src/nodes/node.py
+++ src/nodes/node.py
@@ -22,7 +22,6 @@ class Node:
         # Attributes to fill later
         self.certificate_label: int = None
         self.parent: Node = None
-        self.children: list[Node] = []
 
     def __eq__(self, other: "Node") -> bool:
         """
@@ -81,78 +80,6 @@ class Node:
         """
 
         self.certificate_label = certificate_label
-
-    def add_child(self, child: "Node") -> None:
-        """
-        Add a Node to the `self.children` list.
-
-        Parameters
-        ----------
-        child : Node
-            The Node to be added.
-        """
-
-        self.children.append(child)
-
-    def remove_child(self, child_node: "Node") -> None:
-        """
-        Remove the `child_node` from the the `self.children` list.
-
-        Parameters
-        ----------
-        child_node : Node
-            The Node to remove.
-        """
-
-        if child_node in self.children:
-            self.children.remove(child_node)
-            child_node.parent = None
-
-    def merge(
-        self, merge_target: 'Node', attribute_absortion: dict[str, bool] = {}
-    ) -> None:
-        """
-        Merge the `merge_target` to `self`.
-
-        In this merger, the `merge_target`'s children becomes children of
-        `self`, and the `merge_target` is removed from the tree. If
-        `absorb_value` is set to `True`, then `self.value` is set to
-        `merge_target.value`.
-
-        Parameters
-        ----------
-        merge_target : Node
-            The `Node` to be merged with `self`.
-        attribute_absortion : dict (str: bool)
-            A dictionary of settings to handle the attributes of the node being
-            merged into `self`. This dictionary may set the following keys:
-                - `absorb_value`:
-                    Whether `self.value` should be set to `merge_target.value`.
-                - `parent_children_first`:
-                    Whether `self.children` must come before `merge_target.children`
-                    when absorbing the target's children into the Node's self
-                    children. Useful when handling conditional nodes.
-        """
-
-        absorb_value = attribute_absortion.get("absorb_value", False)
-
-        if absorb_value:
-            self.value = merge_target.value
-
-        for child in merge_target.children:
-            child.add_parent(self)
-
-        parent_children_first = attribute_absortion.get(
-            "parent_children_first",
-            True
-        )
-
-        if parent_children_first:
-            self.children.extend(merge_target.children)
-        else:
-            self.children = [*merge_target.children, *self.children]
-
-        self.children.remove(merge_target)
 
     def add_parent(self, parent: "Node") -> None:
         """
