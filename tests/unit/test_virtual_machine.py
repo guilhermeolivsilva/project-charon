f1da1735a772e2cfbff0f24934e4879a22498ad3
Guilherme Silva <guilherme.olivsilva01@gmail.com>
2024-05-12 18:58:02 -0300

    feat! (virtual_machine): overhauled the instrucion handling for it to be compatible with new nodes infrastructure

--- tests/unit/test_virtual_machine.py
+++ tests/unit/test_virtual_machine.py
@@ -1,16 +1,26 @@
 """Implement unit tests for the `src.virtual_machine.VirtualMachine` class."""
 
-# Just to annotate functions with fixtures.
-from pytest_mock.plugin import MockerFixture
-
-from src.node import Node
 from src.virtual_machine import VirtualMachine
+
+
+HALT_INSTRUCTION = {
+    "instruction": "HALT",
+    "id": -1,
+    "value": None
+}
 
 
 def test_init() -> None:
     """Test the instantiation of VirtualMachine objects."""
 
-    code_collection = [("IFETCH", Node(id=1, kind="VAR", value=0))]
+    code_collection = [
+        {
+            "instruction": "IFETCH",
+            "id": 1,
+            "value": "a"
+        },
+        HALT_INSTRUCTION
+    ]
     stack_size = 10
 
     vm = VirtualMachine(code_collection=code_collection, stack_size=stack_size)
@@ -32,361 +42,473 @@ def test_run() -> None:
     ...
 
 
-def test_run_ifetch(mocker: MockerFixture) -> None:
-    """Test `VirtualMachine.ifetch` through the `run` method."""
+def test_run_ifetch() -> None:
+    """Test the `ifetch` instruction handling with the `run` method."""
 
     test_value = 23
-    test_node = Node(id=1, kind="VAR", value=0)
+    code_collection = [
+        {
+            "instruction": "IFETCH",
+            "id": 1,
+            "value": "a"
+        },
+        HALT_INSTRUCTION
+    ]
 
-    vm = VirtualMachine(
-        code_collection=[
-            ("IFETCH", test_node),
-            ("HALT", None)
-        ],
-        stack_size=1
-    )
-    vm.variables[0] = test_value
+    vm = VirtualMachine(code_collection=code_collection, stack_size=1)
+    vm.variables = {"a": test_value}
 
-    vm.ifetch = mocker.spy(vm, "ifetch")
     vm.run()
 
     # Assert the method was called and the `stack` has the expected value
-    vm.ifetch.assert_called_once_with(test_node)
     assert vm.stack == [test_value]
 
 
-def test_run_ipush(mocker: MockerFixture) -> None:
-    """Test `VirtualMachine.ipush` through the `run` method."""
+def test_run_ipush() -> None:
+    """Test the `ipush` instruction handling with the `run` method."""
 
     test_value = 23
-    test_node = Node(id=1, kind="CST", value=test_value)
 
-    vm = VirtualMachine(
-        code_collection=[
-            ("IPUSH", test_node),
-            ("HALT", None)
-        ],
-        stack_size=1
-    )
+    code_collection = [
+        {
+            "instruction": "IPUSH",
+            "id": 1,
+            "value": test_value
+        },
+        HALT_INSTRUCTION
+    ]
 
-    vm.ipush = mocker.spy(vm, "ipush")
+    vm = VirtualMachine(code_collection=code_collection, stack_size=1)
+
     vm.run()
-
-    # Assert the method was called and the `stack` has the expected value
-    vm.ipush.assert_called_once_with(test_node)
     assert vm.stack == [test_value]
 
 
-def test_run_istore(mocker: MockerFixture) -> None:
-    """Test `VirtualMachine.istore` through the `run` method."""
+def test_run_istore() -> None:
+    """Test the `istore` instruction handling with the `run` method."""
 
     test_value = 23
-    test_node = Node(id=1, kind="VAR", value=0)
+    test_variable = "a"
 
-    vm = VirtualMachine(
-        code_collection=[
-            ("ISTORE", test_node),
-            ("HALT", None)
-        ],
-        stack_size=1
-    )
+    code_collection = [
+        {
+            "instruction": "ISTORE",
+            "id": 1,
+            "value": test_variable
+        },
+        HALT_INSTRUCTION
+    ]
 
+    vm = VirtualMachine(code_collection=code_collection, stack_size=1)
     vm.stack = [test_value]
-    vm.istore = mocker.spy(vm, "istore")
+
     vm.run()
 
-    # Assert the method was called and the `variables` has the expected value
-    vm.istore.assert_called_once_with(test_node)
-    assert vm.variables[0] == test_value
+    assert vm.stack == [test_value]
+    assert vm.variables == {test_variable: test_value}
 
 
-def test_run_ipop(mocker: MockerFixture) -> None:
-    """Test `VirtualMachine.ipop` through the `run` method."""
+def test_run_ipop() -> None:
+    """Test the `ipop` instruction handling with the `run` method."""
 
     test_value = 23
 
-    vm = VirtualMachine(
-        code_collection=[
-            ("IPOP", None),
-            ("HALT", None)
-        ],
-        stack_size=1
-    )
+    code_collection = [
+        {
+            "instruction": "IPOP",
+            "id": 1,
+            "value": None
+        },
+        HALT_INSTRUCTION
+    ]
 
-    # Mock the stack to only contain the `test_value`
+    vm = VirtualMachine(code_collection=code_collection, stack_size=1)
+
+    # Mock the stack to only contain the `test_value`, and point the
+    # `stack_pointer` to it
     vm.stack.append(test_value)
     vm.stack_pointer = 1
-    vm.ipop = mocker.spy(vm, "ipop")
     vm.run()
 
-    # Assert the method was called and the `test_value` has been removed from
-    # the stack
-    vm.ipop.assert_called_once()
+    # Assert the `test_value` has been removed from the stack
     assert vm.stack_pointer == 0
     assert test_value not in vm.stack
 
 
-def test_run_iadd(mocker: MockerFixture) -> None:
-    """Test `VirtualMachine.iadd` through the `run` method."""
+def test_run_iadd() -> None:
+    """Test the `iadd` instruction handling with the `run` method."""
 
     lhs_value = 23
-    lhs = Node(id=1, kind="CST", value=lhs_value)
+    lhs = {
+        "instruction": "IPUSH",
+        "id": 1,
+        "value": lhs_value
+    }
 
     rhs_value = 35
-    rhs = Node(id=2, kind="CST", value=rhs_value)
+    rhs = {
+        "instruction": "IPUSH",
+        "id": 2,
+        "value": rhs_value
+    }
 
-    vm = VirtualMachine(
-        code_collection=[
-            ("IPUSH", lhs),
-            ("IPUSH", rhs),
-            ("IADD", None),
-            ("HALT", None)
-        ],
-        stack_size=2
-    )
+    iadd_instruction = {
+        "instruction": "IADD",
+        "id": 3,
+        "value": None
+    }
 
-    vm.iadd = mocker.spy(vm, "iadd")
+    code_collection = [
+        lhs,
+        rhs,
+        iadd_instruction,
+        HALT_INSTRUCTION
+    ]
+
+    vm = VirtualMachine(code_collection=code_collection, stack_size=2)
     vm.run()
 
-    vm.iadd.assert_called_once()
     assert vm.stack == [lhs_value + rhs_value, rhs_value]
 
 
-def test_run_isub(mocker: MockerFixture) -> None:
-    """Test `VirtualMachine.isub` through the `run` method."""
+def test_run_isub() -> None:
+    """Test the `isub` instruction handling with the `run` method."""
 
     lhs_value = 23
-    lhs = Node(id=1, kind="CST", value=lhs_value)
+    lhs = {
+        "instruction": "IPUSH",
+        "id": 1,
+        "value": lhs_value
+    }
 
     rhs_value = 35
-    rhs = Node(id=2, kind="CST", value=rhs_value)
+    rhs = {
+        "instruction": "IPUSH",
+        "id": 2,
+        "value": rhs_value
+    }
 
-    vm = VirtualMachine(
-        code_collection=[
-            ("IPUSH", lhs),
-            ("IPUSH", rhs),
-            ("ISUB", None),
-            ("HALT", None)
-        ],
-        stack_size=2
-    )
+    isub_instruction = {
+        "instruction": "ISUB",
+        "id": 3,
+        "value": None
+    }
 
-    vm.isub = mocker.spy(vm, "isub")
+    code_collection = [
+        lhs,
+        rhs,
+        isub_instruction,
+        HALT_INSTRUCTION
+    ]
+
+    vm = VirtualMachine(code_collection=code_collection, stack_size=2)
     vm.run()
 
-    vm.isub.assert_called_once()
     assert vm.stack == [lhs_value - rhs_value, rhs_value]
 
 
-def test_run_ilt(mocker: MockerFixture) -> None:
-    """Test `VirtualMachine.ilt` through the `run` method."""
+def test_run_ilt() -> None:
+    """Test the `ilt` instruction handling with the `run` method."""
 
     lhs_value = 23
-    lhs = Node(id=1, kind="CST", value=lhs_value)
+    lhs = {
+        "instruction": "IPUSH",
+        "id": 1,
+        "value": lhs_value
+    }
 
     rhs_value = 35
-    rhs = Node(id=2, kind="CST", value=rhs_value)
+    rhs = {
+        "instruction": "IPUSH",
+        "id": 2,
+        "value": rhs_value
+    }
 
-    vm = VirtualMachine(
-        code_collection=[
-            ("IPUSH", lhs),
-            ("IPUSH", rhs),
-            ("ILT", None),
-            ("HALT", None)
-        ],
-        stack_size=2
-    )
+    ilt_instruction = {
+        "instruction": "ILT",
+        "id": 3,
+        "value": None
+    }
 
-    vm.ilt = mocker.spy(vm, "ilt")
+    code_collection = [
+        lhs,
+        rhs,
+        ilt_instruction,
+        HALT_INSTRUCTION
+    ]
+
+    vm = VirtualMachine(code_collection=code_collection, stack_size=2)
     vm.run()
 
-    vm.ilt.assert_called_once()
     assert vm.stack == [lhs_value < rhs_value, rhs_value]
 
 
-def test_run_jmp(mocker: MockerFixture) -> None:
-    """Test `VirtualMachine.jmp` through the `run` method."""
+def test_run_jmp() -> None:
+    """Test the `jmp` instruction handling with the `run` method."""
 
-    node_to_run = Node(id=1, kind="CST", value=23)
-    node_to_ignore = Node(id=2, kind="CST", value=35)
-    another_node_to_run = Node(id=3, kind="CST", value=13)
+    ran_instruction_1_value = 23
+    ignored_instruction_values = 35
+    ran_instruction_2_value = 13
 
-    vm = VirtualMachine(
-        code_collection=[
-            ("IPUSH", node_to_run),
-            ("JMP", another_node_to_run),
-            ("IPUSH", node_to_ignore),
-            ("IPUSH", another_node_to_run),
-            ("HALT", None)
-        ],
-        stack_size=3
-    )
+    instruction_to_run = {
+        "instruction": "IPUSH",
+        "id": 1,
+        "value": 23
+    }
 
-    vm.jmp = mocker.spy(vm, "jmp")
+    instruction_to_ignore = {
+        "instruction": "IPUSH",
+        "id": 2,
+        "value": 35
+    }
+
+    another_instruction_to_run = {
+        "instruction": "IPUSH",
+        "id": 3,
+        "value": 13
+    }
+
+    jmp_instruction = {
+        "instruction": "JMP",
+        "id": 3,
+        "value": None
+    }
+
+    code_collection = [
+        instruction_to_run,
+        jmp_instruction,
+        instruction_to_ignore,
+        another_instruction_to_run,
+        HALT_INSTRUCTION
+    ]
+
+    vm = VirtualMachine(code_collection=code_collection, stack_size=3)
 
     vm.run()
 
-    vm.jmp.assert_called_once()
-    assert vm.stack == [node_to_run.value, another_node_to_run.value, None]
+    assert ran_instruction_1_value in vm.stack
+    assert ran_instruction_2_value in vm.stack
+    assert ignored_instruction_values not in vm.stack
 
 
-def test_run_jz_true(mocker: MockerFixture) -> None:
-    """
-    Test `VirtualMachine.jz` through the `run` method.
+def test_run_jz_true() -> None:
+    """Test the `jz` instruction handling with the `run` method.
 
     In this test, assert that the `jz` method correctly handles `True`
     conditions.
     """
 
-    condition_node = Node(id=1, kind="CST", value=True)
-    node_not_to_ignore = Node(id=2, kind="CST", value=35)
-    another_node_to_run = Node(id=3, kind="CST", value=13)
+    condition_instruction = {
+        "instruction": "IPUSH",
+        "id": 1,
+        "value": True
+    }
 
-    vm = VirtualMachine(
-        code_collection = [
-            ("IPUSH", condition_node),
-            ("JZ", another_node_to_run),
-            ("IPUSH", node_not_to_ignore),
-            ("IPUSH", another_node_to_run),
-            ("HALT", None)
-        ],
-        stack_size=3
-    )
+    instruction_to_run = {
+        "instruction": "IPUSH",
+        "id": 2,
+        "value": 23
+    }
 
-    vm.jz = mocker.spy(vm, "jz")
+    another_instruction_to_run = {
+        "instruction": "IPUSH",
+        "id": 3,
+        "value": 35
+    }
+
+    conditional_jump_instruction = {
+        "instruction": "JZ",
+        "id": 3,
+        "value": None
+    }
+
+    code_collection = [
+        condition_instruction,
+        conditional_jump_instruction,
+        instruction_to_run,
+        another_instruction_to_run,
+        HALT_INSTRUCTION
+    ]
+
+    vm = VirtualMachine(code_collection=code_collection, stack_size=3)
 
     vm.run()
 
-    vm.jz.assert_called_once()
-
     assert vm.stack == [
-        condition_node.value,
-        node_not_to_ignore.value,
-        another_node_to_run.value
+        condition_instruction["value"],
+        instruction_to_run["value"],
+        another_instruction_to_run["value"]
     ]
 
 
-def test_run_jz_false(mocker: MockerFixture) -> None:
-    """
-    Test `VirtualMachine.jz` through the `run` method.
+def test_run_jz_false() -> None:
+    """Test the `jz` instruction handling with the `run` method.
 
     In this test, assert that the `jz` method correctly handles `False`
     conditions.
     """
 
-    condition_node = Node(id=1, kind="CST", value=False)
-    node_to_ignore = Node(id=2, kind="CST", value=35)
-    another_node_to_run = Node(id=3, kind="CST", value=13)
+    condition_instruction = {
+        "instruction": "IPUSH",
+        "id": 1,
+        "value": False
+    }
 
-    vm = VirtualMachine(
-        code_collection = [
-            ("IPUSH", condition_node),
-            ("JZ", another_node_to_run),
-            ("IPUSH", node_to_ignore),
-            ("IPUSH", another_node_to_run),
-            ("HALT", None)
-        ],
-        stack_size=3
-    )
+    instruction_to_ignore = {
+        "instruction": "IPUSH",
+        "id": 2,
+        "value": 23
+    }
 
-    vm.jz = mocker.spy(vm, "jz")
+    instruction_to_run = {
+        "instruction": "IPUSH",
+        "id": 3,
+        "value": 35
+    }
+
+    conditional_jump_instruction = {
+        "instruction": "JZ",
+        "id": 3,
+        "value": None
+    }
+
+    code_collection = [
+        condition_instruction,
+        conditional_jump_instruction,
+        instruction_to_ignore,
+        instruction_to_run,
+        HALT_INSTRUCTION
+    ]
+
+    vm = VirtualMachine(code_collection=code_collection, stack_size=3)
 
     vm.run()
 
-    vm.jz.assert_called_once()    
     assert vm.stack == [
-        condition_node.value,
-        another_node_to_run.value,
+        condition_instruction["value"],
+        instruction_to_run["value"],
         None
     ]
 
+    assert instruction_to_ignore["value"] not in vm.stack
 
-def test_run_jnz_true(mocker: MockerFixture) -> None:
-    """
-    Test `VirtualMachine.jnz` through the `run` method.
+
+def test_run_jnz_true() -> None:
+    """Test the `jnz` instruction handling with the `run` method.
 
     In this test, assert that the `jnz` method correctly handles `True`
     conditions.
     """
 
-    condition_node = Node(id=1, kind="CST", value=True)
-    node_to_ignore = Node(id=2, kind="CST", value=35)
-    another_node_to_run = Node(id=3, kind="CST", value=13)
+    condition_instruction = {
+        "instruction": "IPUSH",
+        "id": 1,
+        "value": True
+    }
 
-    vm = VirtualMachine(
-        code_collection = [
-            ("IPUSH", condition_node),
-            ("JNZ", another_node_to_run),
-            ("IPUSH", node_to_ignore),
-            ("IPUSH", another_node_to_run),
-            ("HALT", None)
-        ],
-        stack_size=3
-    )
+    instruction_to_ignore = {
+        "instruction": "IPUSH",
+        "id": 2,
+        "value": 23
+    }
 
-    vm.jnz = mocker.spy(vm, "jnz")
+    instruction_to_run = {
+        "instruction": "IPUSH",
+        "id": 3,
+        "value": 35
+    }
+
+    conditional_jump_instruction = {
+        "instruction": "JNZ",
+        "id": 3,
+        "value": None
+    }
+
+    code_collection = [
+        condition_instruction,
+        conditional_jump_instruction,
+        instruction_to_ignore,
+        instruction_to_run,
+        HALT_INSTRUCTION
+    ]
+
+    vm = VirtualMachine(code_collection=code_collection, stack_size=3)
 
     vm.run()
 
-    vm.jnz.assert_called_once()    
     assert vm.stack == [
-        condition_node.value,
-        another_node_to_run.value,
+        condition_instruction["value"],
+        instruction_to_run["value"],
         None
     ]
 
+    assert instruction_to_ignore["value"] not in vm.stack
 
-def test_run_jnz_false(mocker: MockerFixture) -> None:
-    """
-    Test `VirtualMachine.jnz` through the `run` method.
+
+def test_run_jnz_false() -> None:
+    """Test the `jnz` instruction handling with the `run` method.
 
     In this test, assert that the `jnz` method correctly handles `False`
     conditions.
     """
 
-    condition_node = Node(id=1, kind="CST", value=False)
-    node_not_to_ignore = Node(id=2, kind="CST", value=35)
-    another_node_to_run = Node(id=3, kind="CST", value=13)
+    condition_instruction = {
+        "instruction": "IPUSH",
+        "id": 1,
+        "value": False
+    }
 
-    vm = VirtualMachine(
-        code_collection = [
-            ("IPUSH", condition_node),
-            ("JNZ", another_node_to_run),
-            ("IPUSH", node_not_to_ignore),
-            ("IPUSH", another_node_to_run),
-            ("HALT", None)
-        ],
-        stack_size=3
-    )
+    instruction_to_run = {
+        "instruction": "IPUSH",
+        "id": 2,
+        "value": 23
+    }
 
-    vm.jnz = mocker.spy(vm, "jnz")
+    another_instruction_to_run = {
+        "instruction": "IPUSH",
+        "id": 3,
+        "value": 35
+    }
+
+    conditional_jump_instruction = {
+        "instruction": "JNZ",
+        "id": 3,
+        "value": None
+    }
+
+    code_collection = [
+        condition_instruction,
+        conditional_jump_instruction,
+        instruction_to_run,
+        another_instruction_to_run,
+        HALT_INSTRUCTION
+    ]
+
+    vm = VirtualMachine(code_collection=code_collection, stack_size=3)
 
     vm.run()
 
-    vm.jnz.assert_called_once()
-
     assert vm.stack == [
-        condition_node.value,
-        node_not_to_ignore.value,
-        another_node_to_run.value
+        condition_instruction["value"],
+        instruction_to_run["value"],
+        another_instruction_to_run["value"]
     ]
 
 
-def test_run_empty(mocker: MockerFixture) -> None:
-    """Test `VirtualMachine.jnz` through the `run` method."""
+def test_run_empty() -> None:
+    """Test the `empty` instruction handling with the `run` method."""
 
-    vm = VirtualMachine(
-        code_collection=[
-            ("EMPTY", Node(id=-1, kind="EMPTY")),
-            ("HALT", None)
-        ],
-        stack_size=1
-    )
+    code_collection = [
+        {
+            "instruction": "EMPTY",
+            "id": 1,
+            "value": None
+        },
+        HALT_INSTRUCTION
+    ]
 
-    vm.empty = mocker.spy(vm, "empty")
+    vm = VirtualMachine(code_collection, stack_size=1)
 
     vm.run()
-
-    vm.empty.assert_called_once()
     assert vm.stack == [None]
 
