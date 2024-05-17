bc0c39874ee17f8ed02e21dc77e28bd719458673
Guilherme Silva <guilherme.olivsilva01@gmail.com>
2024-05-17 15:41:17 -0300

    chore!: stripped down certificator interface

--- src/certificators/interface.py
+++ src/certificators/interface.py
@@ -1,19 +1,10 @@
 """Base class for Certificator classes (i.e., back and frontends)."""
 
 from abc import abstractmethod
-from string import ascii_lowercase
 
 
 class Interface:
     """Interface for certificator classes."""
-
-    variables = ascii_lowercase
-    literals = [str(literal) for literal in range(0, 10)]
-
-    basic_symbols = [
-        *variables,
-        *literals
-    ]
 
     @abstractmethod
     def __init__(self, code) -> None:
