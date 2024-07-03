d78e1274fd67bf46268e0c38c416e5b42bf260f9
Guilherme Silva <guilherme.olivsilva01@gmail.com>
2024-07-03 19:17:12 -0300

    test: updated Lexer unit test to reflect latest changes

--- tests/unit/test_lexer.py
+++ tests/unit/test_lexer.py
@@ -1,47 +1,94 @@
 """Implement unit tests for the `src.lexer.Lexer` class."""
 
 import pytest
-from string import ascii_lowercase
 
 from src.lexer import Lexer
 
 
 SOURCE_CODE = """
-    int main()
-    {
-        struct my_struct {
-            int x[5];
-            float y;
-            long z;
-        };
+int a = 1;
 
-        int a;
-        my_struct b;
+struct my_struct {
+    int x;
+    float y;
+};
 
-        b.x = {1, 2, 3, 4, 5};
+my_struct global_var;
 
-        do {
-            if (a < b.x) { a = a + 1; }
-            else { b.y = b.y - 1.0; }
-        }
-        while (a < 10);
+int abc(int asda) {
+    int bla = 1;
+    float blabla = 2.0;
+    long int xaxaxa;
+
+    my_struct internal_struct_var;
+    internal_struct_var.x = 1;
+
+    bla = bla + function_that_returns_struct(blabla);
+
+    return blabla + bla;
+}
+
+my_struct function_that_returns_struct(int xyz) {
+    return xyz;
+}
+
+struct test_struct {
+    int abcd;
+    int xyz;
+}
+
+int main() {
+    int x = abc();
+    int array[10];
+
+    array[5] = 1;
+
+    if(((x << 4) == 1 or x > 1) and (x < 10)) {
+        int y = x & 1;
     }
+    else {
+        int y = x | 1;
+    }
+
+    return ((x * y) / 2) >> 1;
+}
 """
 
+INVALID_STRUCTS = [
+    # 1. Struct named after reserved word/symbol
+    "struct int { int a; };",
+
+    # 2. Struct attribute named after reserved word
+    "struct abc { int float; };",
+
+    # Struct attribute redefinition
+    "struct abc { int a; int a; };",
+
+    # Struct attribute with unknown type
+    "struct abc { int a; unknown_type b; };"
+]
+
+INVALID_VARIABLES = [
+    # 1. Variable with unknown type
+    "unknown_type var;",
+
+    # 2. Invalid variable definition
+    "int var ." 
+]
+
+INVALID_FUNCTIONS = [
+    # 1. Call of undefined function
+    "int main() { int abc; abc = func(); }",
+
+    # 2. Malformed function definitions
+    "int abc(int) { return x; }",
+]
+
+
 INVALID_SOURCES = [
-    # 1. Variable named after reserved word/symbol.
-    "int int;",
-    "float return;",
-    "int {",
-
-    # 2. Variable named after user-defined type.
-    "struct abc { int x; }; int abc;",
-
-    # 3. Multiple variables in a row without operator in between.
-    "int a; int b; a b;",
-
-    # 4. Unnamed struct.
-    "struct { int x; };"
+    *INVALID_STRUCTS,
+    *INVALID_VARIABLES,
+    *INVALID_FUNCTIONS
 ]
 
 
@@ -52,264 +99,326 @@ def test_parse_source_code():
     This test uses a snippet that uses all reserved words.
     """
 
-    expected_parsed_code = [
-        ('INT_TYPE', None),
-        ('FUNC', 'main'),
-        ('LPAR', None),
-        ('RPAR', None),
-        ('LCBRA', None),
-        ('STRUCT_DEF', 'my_struct'),
-        ('LCBRA', None),
-        ('INT_TYPE', None),
-        ('ID', 'x'),
-        ('LBRA', None),
-        ('ARRAY_SIZE', 5),
-        ('RBRA', None),
-        ('SEMI', None),
-        ('FLOAT_TYPE', None),
-        ('ID', 'y'),
-        ('SEMI', None),
-        ('LONG_TYPE', None),
-        ('ID', 'z'),
-        ('SEMI', None),
-        ('RCBRA', None),
-        ('SEMI', None),
-        ('INT_TYPE', None),
-        ('ID', 'a'),
-        ('SEMI', None),
-        ('STRUCT_TYPE', 'my_struct'),
-        ('ID', 'b'),
-        ('SEMI', None),
-        ('ID', 'b'),
-        ('DOT', None),
-        ('PROP', 'x'),
-        ('EQUAL', None),
-        ('LCBRA', None),
-        ('INT', 1),
-        ('INT', 2),
-        ('INT', 3),
-        ('INT', 4),
-        ('INT', 5),
-        ('RCBRA', None),
-        ('SEMI', None),
-        ('DO_SYM', None),
-        ('LCBRA', None),
-        ('IF_SYM', None),
-        ('LPAR', None),
-        ('ID', 'a'),
-        ('LESS', None),
-        ('ID', 'b'),
-        ('DOT', None),
-        ('PROP', 'x'),
-        ('RPAR', None),
-        ('LCBRA', None),
-        ('ID', 'a'),
-        ('EQUAL', None),
-        ('ID', 'a'),
-        ('PLUS', None),
-        ('INT', 1),
-        ('SEMI', None),
-        ('RCBRA', None),
-        ('ELSE_SYM', None),
-        ('LCBRA', None),
-        ('ID', 'b'),
-        ('DOT', None),
-        ('PROP', 'y'),
-        ('EQUAL', None),
-        ('ID', 'b'),
-        ('DOT', None),
-        ('PROP', 'y'),
-        ('MINUS', None),
-        ('FLOAT', 1.0),
-        ('SEMI', None),
-        ('RCBRA', None),
-        ('RCBRA', None),
-        ('WHILE_SYM', None),
-        ('LPAR', None),
-        ('ID', 'a'),
-        ('LESS', None),
-        ('INT', 10),
-        ('RPAR', None),
-        ('SEMI', None),
-        ('RCBRA', None)
-    ]
+    expected_parsed_code = {
+        'globals': {
+            'variables': {
+                'a': {'type': 'int'},
+                'global_var': {'type': 'my_struct'}
+            },
+            'structs': {
+                'my_struct': {'x': 'int', 'y': 'float'},
+                'test_struct': {'abcd': 'int', 'xyz': 'int'}
+            }
+        },
+        'functions': {
+            'abc': {
+                'type': 'int',
+                'arguments': {'asda': 'int'},
+                'statements': [
+                    ('VAR_DEF', {'name': 'bla', 'type': 'int'}),
+                    'ASSIGN',
+                    ('CST', {'type': 'int', 'value': 1}),
+                    'SEMI',
+                    ('VAR_DEF', {'name': 'blabla', 'type': 'float'}),
+                    'ASSIGN',
+                    ('CST', {'type': 'float', 'value': 2.0}),
+                    'SEMI',
+                    ('VAR_DEF', {'name': 'xaxaxa', 'type': 'long'}),
+                    'SEMI',
+                    ('VAR_DEF', {'name': 'internal_struct_var', 'type': 'my_struct'}),
+                    'SEMI',
+                    ('VAR', 'internal_struct_var'),
+                    'DOT',
+                    ('STRUCT_ATTR', 'x'),
+                    'ASSIGN',
+                    ('CST', {'type': 'int', 'value': 1}),
+                    'SEMI',
+                    ('VAR', 'bla'),
+                    'ASSIGN',
+                    ('VAR', 'bla'),
+                    'PLUS',
+                    ('FUNC_CALL',
+                    {'function': 'function_that_returns_struct', 'parameters': ['blabla']}),
+                    'LPAR',
+                    ('VAR', 'blabla'),
+                    'RPAR',
+                    'SEMI',
+                    'RET_SYM',
+                    ('VAR', 'blabla'),
+                    'PLUS',
+                    ('VAR', 'bla'),
+                    'SEMI'
+                ]
+            },
+            'function_that_returns_struct': {
+                'type': 'my_struct',
+                'arguments': {'xyz': 'int'},
+                'statements': [
+                    'RET_SYM',
+                    ('VAR', 'xyz'),
+                    'SEMI'
+                ]
+            },
+            'main': {
+                'type': 'int',
+                'arguments': {},
+                'statements': [
+                    ('VAR_DEF', {'name': 'x', 'type': 'int'}),
+                    'ASSIGN',
+                    ('FUNC_CALL', {'function': 'abc', 'parameters': []}),
+                    'LPAR',
+                    'RPAR',
+                    'SEMI',
+                    ('VAR_DEF', {'name': 'array', 'type': 'int', 'length': 10}),
+                    'LBRA',
+                    ('CST', {'type': 'int', 'value': 10}),
+                    'RBRA',
+                    'SEMI',
+                    ('VAR', 'array'),
+                    'LBRA',
+                    ('CST', {'type': 'int', 'value': 5}),
+                    'RBRA',
+                    'ASSIGN',
+                    ('CST', {'type': 'int', 'value': 1}),
+                    'SEMI',
+                    'IF_SYM',
+                    'LPAR',
+                    'LPAR',
+                    'LPAR',
+                    ('VAR', 'x'),
+                    'LSHIFT',
+                    ('CST', {'type': 'int', 'value': 4}),
+                    'RPAR',
+                    'EQUAL',
+                    ('CST', {'type': 'int', 'value': 1}),
+                    'OR',
+                    ('VAR', 'x'),
+                    'GREATER',
+                    ('CST', {'type': 'int', 'value': 1}),
+                    'RPAR',
+                    'AND',
+                    'LPAR',
+                    ('VAR', 'x'),
+                    'LESS',
+                    ('CST', {'type': 'int', 'value': 10}),
+                    'RPAR',
+                    'RPAR',
+                    'LCBRA',
+                    ('VAR_DEF', {'name': 'y', 'type': 'int'}),
+                    'ASSIGN',
+                    ('VAR', 'x'),
+                    'BITAND',
+                    ('CST', {'type': 'int', 'value': 1}),
+                    'SEMI',
+                    'RCBRA',
+                    'ELSE_SYM',
+                    'LCBRA',
+                    ('VAR', 'y'),
+                    'ASSIGN',
+                    ('VAR', 'x'),
+                    'BITOR',
+                    ('CST', {'type': 'int', 'value': 1}),
+                    'SEMI',
+                    'RCBRA',
+                    'RET_SYM',
+                    'LPAR',
+                    'LPAR',
+                    ('VAR', 'x'),
+                    'MULT',
+                    ('VAR', 'y'),
+                    'RPAR',
+                    'DIV',
+                    ('CST', {'type': 'int', 'value': 2}),
+                    'RPAR',
+                    'RSHIFT',
+                    ('CST', {'type': 'int', 'value': 1}),
+                    'SEMI'
+                ]
+            }
+        }
+    }
 
     lexer = Lexer(SOURCE_CODE)
     lexer_parsed_code = lexer.parse_source_code()
 
-    assert list(lexer_parsed_code) == expected_parsed_code
+    assert lexer_parsed_code == expected_parsed_code
 
 
-@pytest.mark.parametrize(
-    "word_tuple",
-    [
-        ("int", ("INT_TYPE", None)),
-        ("float", ("FLOAT_TYPE", None)),
-        ("long", ("LONG_TYPE", None)),
-        ("struct", ("STRUCT_DEF", None)),
-        ("do", ("DO_SYM", None)),
-        ("while", ("WHILE_SYM", None)),
-        ("if", ("IF_SYM", None)),
-        ("else", ("ELSE_SYM", None)),
-        ("return", ("RET_SYM", None)),
-        ("{", ("LCBRA", None)),
-        ("[", ("LBRA", None)),
-        ("]", ("RBRA", None)),
-        ("}", ("RCBRA", None)),
-        ("(", ("LPAR", None)),
-        (")", ("RPAR", None)),
-        ("+", ("PLUS", None)),
-        ("-", ("MINUS", None)),
-        ("<", ("LESS", None)),
-        (";", ("SEMI", None)),
-        ("=", ("EQUAL", None)),
-        (".", ("DOT", None))
-    ]
-)
-def test_parse_word_symbols_and_words(word_tuple):
+def test_split_source():
     """
-    Test the `Lexer.parse_word` method with all the reserved words and symbols.
-
-    Parameters
-    ----------
-    word_tuple : tuple
-        A tuple of (word_to_parse, expected_symbol).
-    """
-
-    lexer = Lexer(None)
-
-    word_to_parse, expected_symbol = word_tuple
-    assert expected_symbol == lexer.parse_word(word_to_parse)
-
-
-@pytest.mark.parametrize(
-    "variable_tuple",
-    [(f"var_{letter}", ("ID", letter)) for letter in ascii_lowercase]
-)
-def test_parse_word_variables(variable_tuple):
-    """
-    Test the `Lexer.parse_word` method with all the variables (i.e., a-z).
-
-    Parameters
-    ----------
-    variable_tuple : tuple
-        A tuple of (variable_to_parse, expected_symbol).
-    """
-
-    lexer = Lexer(None)
-
-    variable_to_parse, expected_symbol = variable_tuple
-    assert expected_symbol == lexer.parse_word(variable_to_parse)
-
-
-@pytest.mark.parametrize(
-    "literal_tuple",
-    [
-        *[(f"int_{str(i)}", ("INT", i)) for i in range(0, 10)],
-        *[(f"float_{str(i)}.0", ("FLOAT", float(i))) for i in range(0, 10)]
-    ]
-)
-def test_parse_word_literals(literal_tuple):
-    """
-    Test the `Lexer.parse_word` method with all the basic literals (0-9).
-
-    Parameters
-    ----------
-    literal_tuple : tuple
-        A tuple of (literal_to_parse, expected_symbol).
-    """
-
-    lexer = Lexer(None)
-
-    literal_to_parse, expected_symbol = literal_tuple
-    assert expected_symbol == lexer.parse_word(literal_to_parse)
-
-
-def test_tokenize_source_code():
-    """
-    Test the `Lexer.tokenize_source_code` method.
+    Test the `Lexer.split_source` method.
 
     This test uses a snippet that uses all reserved words.
     """
 
-    expected_tokenized_code = [
+    expected_split_source = [
         'int',
-        'func_main',
-        '(',
-        ')',
-        '{',
+        'a',
+        '=',
+        '1',
+        ';',
         'struct',
-        'struct_my_struct',
+        'my_struct',
         '{',
         'int',
-        'var_x',
-        '[',
-        'int_5',
-        ']',
+        'x',
         ';',
         'float',
-        'var_y',
-        ';',
-        'long',
-        'var_z',
+        'y',
         ';',
         '}',
+        ';',
+        'my_struct',
+        'global_var',
         ';',
         'int',
-        'var_a',
-        ';',
-        'var_my_struct',
-        'var_b',
-        ';',
-        'var_b.x',
-        '=',
-        '{',
-        'int_1',
-        'int_2',
-        'int_3',
-        'int_4',
-        'int_5',
-        '}',
-        ';',
-        'do',
-        '{',
-        'if',
+        'abc',
         '(',
-        'var_a',
-        '<',
-        'var_b.x',
+        'int',
+        'asda',
         ')',
         '{',
-        'var_a',
+        'int',
+        'bla',
         '=',
-        'var_a',
+        '1',
+        ';',
+        'float',
+        'blabla',
+        '=',
+        '2.0',
+        ';',
+        'long',
+        'xaxaxa',
+        ';',
+        'my_struct',
+        'internal_struct_var',
+        ';',
+        'internal_struct_var.x',
+        '=',
+        '1',
+        ';',
+        'bla',
+        '=',
+        'bla',
         '+',
-        'int_1',
+        'function_that_returns_struct',
+        '(',
+        'blabla',
+        ')',
+        ';',
+        'return',
+        'blabla',
+        '+',
+        'bla',
+        ';',
+        '}',
+        'my_struct',
+        'function_that_returns_struct',
+        '(',
+        'int',
+        'xyz',
+        ')',
+        '{',
+        'return',
+        'xyz',
+        ';',
+        '}',
+        'struct',
+        'test_struct',
+        '{',
+        'int',
+        'abcd',
+        ';',
+        'int',
+        'xyz',
+        ';',
+        '}',
+        'int',
+        'main',
+        '(',
+        ')',
+        '{',
+        'int',
+        'x',
+        '=',
+        'abc',
+        '(',
+        ')',
+        ';',
+        'int',
+        'array',
+        '[',
+        '10',
+        ']',
+        ';',
+        'array',
+        '[',
+        '5',
+        ']',
+        '=',
+        '1',
+        ';',
+        'if',
+        '(',
+        '(',
+        '(',
+        'x',
+        '<<',
+        '4',
+        ')',
+        '==',
+        '1',
+        'or',
+        'x',
+        '>',
+        '1',
+        ')',
+        'and',
+        '(',
+        'x',
+        '<',
+        '10',
+        ')',
+        ')',
+        '{',
+        'int',
+        'y',
+        '=',
+        'x',
+        '&',
+        '1',
         ';',
         '}',
         'else',
         '{',
-        'var_b.y',
+        'int',
+        'y',
         '=',
-        'var_b.y',
-        '-',
-        'float_1.0',
+        'x',
+        '|',
+        '1',
         ';',
         '}',
-        '}',
-        'while',
+        'return',
         '(',
-        'var_a',
-        '<',
-        'int_10',
+        '(',
+        'x',
+        '*',
+        'y',
         ')',
+        '/',
+        '2',
+        ')',
+        '>>',
+        '1',
         ';',
         '}'
     ]
 
     lexer = Lexer(SOURCE_CODE)
 
-    assert lexer.tokenize_source_code() == expected_tokenized_code
+    assert lexer.split_source() == expected_split_source
 
 
 @pytest.mark.parametrize(
