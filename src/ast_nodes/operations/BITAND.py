b1f99b9f49f8b435b082226b5a571170780ad8ee
Guilherme Silva <guilherme.olivsilva01@gmail.com>
2024-08-12 17:46:01 -0300

    feat: added BITAND AST node

---
+++ src/ast_nodes/operations/BITAND.py
@@ -0,0 +1,28 @@
+"""Representation of BITAND nodes for the Abstract Syntax Tree."""
+
+from typing_extensions import override
+
+from src.ast_nodes.node import Node
+
+from src.ast_nodes.operations.operation import Operation
+
+
+class BITAND(Operation):
+    """
+    Implement the representation of a bit-wise and operation (&) for the AST.
+
+    Parameters
+    ----------
+    id : int
+        The ID of the Node.
+    lhs : Node
+        The Node representation of the left hand side of the operation.
+    rhs : Node
+        The Node representation of the right hand side of the operation.
+    """
+
+    @override
+    def __init__(self, id: int, lhs: Node, rhs: Node, **kwargs) -> None:
+        super().__init__(id, lhs, rhs, **kwargs)
+
+        self.instruction: str = "BITAND"
