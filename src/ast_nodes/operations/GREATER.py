2a52ba7f04b05ad99adce04c6bd8767966397773
Guilherme Silva <guilherme.olivsilva01@gmail.com>
2024-08-12 16:45:40 -0300

    feat: added GREATER AST node

---
+++ src/ast_nodes/operations/GREATER.py
@@ -0,0 +1,28 @@
+"""Representation of GREATER nodes for the Abstract Syntax Tree."""
+
+from typing_extensions import override
+
+from src.ast_nodes.node import Node
+
+from src.ast_nodes.operations.operation import Operation
+
+
+class GREATER(Operation):
+    """
+    Implement the representation of a "greater than" (>) comparison for the AST.
+
+    Parameters
+    ----------
+    id : int
+        The ID of the Node.
+    lhs : Node
+        The Node representation of the left hand side of the comparison.
+    rhs : Node
+        The Node representation of the right hand side of the comparison.
+    """
+
+    @override
+    def __init__(self, id: int, lhs: Node, rhs: Node, **kwargs) -> None:
+        super().__init__(id, lhs, rhs, **kwargs)
+
+        self.instruction: str = "GT"
