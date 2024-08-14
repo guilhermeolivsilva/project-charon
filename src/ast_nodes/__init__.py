31021753fff0196c2108ca7f22905758ae866fe4
Guilherme Silva <guilherme.olivsilva01@gmail.com>
2024-08-14 20:27:25 -0300

    enhancement: reorganized AST Nodes module init

--- src/ast_nodes/__init__.py
+++ src/ast_nodes/__init__.py
@@ -27,18 +27,20 @@ from src.ast_nodes.conditionals.IF import IF
 from src.ast_nodes.conditionals.IFELSE import IFELSE
 from src.ast_nodes.conditionals.WHILE import WHILE
 
-# Operations
-from src.ast_nodes.operations.AND import AND
-from src.ast_nodes.operations.ADD import ADD
+# Binary Operators
 from src.ast_nodes.operations.ASSIGN import ASSIGN
+from src.ast_nodes.operations.ADD import ADD
+from src.ast_nodes.operations.SUB import SUB
+from src.ast_nodes.operations.MULT import MULT
+from src.ast_nodes.operations.DIV import DIV
+from src.ast_nodes.operations.LSHIFT import LSHIFT
+from src.ast_nodes.operations.RSHIFT import RSHIFT
+from src.ast_nodes.operations.AND import AND
+from src.ast_nodes.operations.OR import OR
 from src.ast_nodes.operations.BITAND import BITAND
 from src.ast_nodes.operations.BITOR import BITOR
-from src.ast_nodes.operations.DIV import DIV
+
+# Comparisons
 from src.ast_nodes.operations.EQUAL import EQUAL
-from src.ast_nodes.operations.GREATER import GREATER
 from src.ast_nodes.operations.LESS import LESS
-from src.ast_nodes.operations.LSHIFT import LSHIFT
-from src.ast_nodes.operations.MULT import MULT
-from src.ast_nodes.operations.OR import OR
-from src.ast_nodes.operations.RSHIFT import RSHIFT
-from src.ast_nodes.operations.SUB import SUB
+from src.ast_nodes.operations.GREATER import GREATER
