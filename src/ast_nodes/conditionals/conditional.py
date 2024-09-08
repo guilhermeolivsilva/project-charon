3c44207f6563cbdc91de524ac662580401f05d34
Guilherme Silva <guilherme.olivsilva01@gmail.com>
2024-09-08 19:57:09 -0300

    feat: updated ast_nodes.conditionals subclasses to use new, LLVM-like register-based code generation

--- src/ast_nodes/conditionals/conditional.py
+++ src/ast_nodes/conditionals/conditional.py
@@ -24,7 +24,7 @@ class Conditional(Node):
     def __init__(
         self, id: int, parenthesis_expression: Node, statement_if_true: Node
     ) -> None:
-        super().__init__(id)
+        super().__init__(id, uses_register=False)
 
         self.parenthesis_expression: Node = parenthesis_expression
         self.statement_if_true: Node = statement_if_true
