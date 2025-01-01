f673ea8f1ebe88ab54cc0a857adc5db8d023ec40
Guilherme Silva <guilherme.olivsilva01@gmail.com>
2025-01-01 18:56:10 -0300

    test: added missing certification integration tests

--- tests/integration/test_struct.py
+++ tests/integration/test_struct.py
@@ -1,5 +1,6 @@
 """Integration test for a simple struct manipulation."""
 
+from src.certificators import BackendCertificator, FrontendCertificator
 from src.runner import create_instance
 
 
@@ -31,3 +32,17 @@ def test_struct():
     expected_memory = {'0x0': 10, '0x4': 10.150331125827813, '0x8': 101}
 
     assert vm.get_memory() == expected_memory
+
+
+def test_struct_certification() -> None:
+    """Test the front and backend certification."""
+
+    instance = create_instance(source_code=SOURCE_CODE)
+
+    ast = instance.get_ast()
+    frontend_certificate = FrontendCertificator(ast=ast).certificate()
+
+    program = instance.get_program()
+    backend_certificate = BackendCertificator(program=program).certificate()
+
+    assert frontend_certificate == backend_certificate
--- tests/integration/test_while.py
+++ tests/integration/test_while.py
@@ -1,5 +1,6 @@
 """Integration test for a simple `while` statement."""
 
+from src.certificators import BackendCertificator, FrontendCertificator
 from src.runner import create_instance
 
 
@@ -26,3 +27,17 @@ def test_simple_while() -> None:
 
     expected_memory = {'0x0': 128}
     assert vm.get_memory() == expected_memory
+
+
+def test_struct_certification() -> None:
+    """Test the front and backend certification."""
+
+    instance = create_instance(source_code=SOURCE_CODE)
+
+    ast = instance.get_ast()
+    frontend_certificate = FrontendCertificator(ast=ast).certificate()
+
+    program = instance.get_program()
+    backend_certificate = BackendCertificator(program=program).certificate()
+
+    assert frontend_certificate == backend_certificate
